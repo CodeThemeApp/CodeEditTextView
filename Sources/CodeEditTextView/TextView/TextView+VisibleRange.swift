5607c512367f64facb1d6349d147c9320751b924
Daniel Choroszucha <daniel.choroszucha@icloud.com>
2025-03-09 17:45:26 +0100

    WIP: Extend TextView with VisibleRange

---
+++ Sources/CodeEditTextView/TextView/TextView+VisibleRange.swift
@@ -0,0 +1,15 @@
+//
+//  TextView+VisibleRange.swift
+//  CodeEditTextView
+//
+//  Created by Daniel Choroszucha on 09/03/2025.
+//
+
+import Foundation
+
+extension TextView {
+    // TODO: [09.03.2025] Calculate visibile range -
+    var visibleRange: NSRange {
+        documentRange
+    }
+}
